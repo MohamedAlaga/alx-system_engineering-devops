4624d36a2d291f9e2c2c9dc30e81a524eafebeed
MohamedAlaga <malaga.142014@gmail.com>
2024-03-22 22:49:30 +0200

    finishing

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
 #!/usr/bin/pup
 #install a flask from pip3
-package { 'flask':
-    ensure   => '2.1.0',
-    provider => 'pip3'
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0 Werkzeug==2.0.3',
+  path    => ['/usr/bin'],
+  # require => Package['python3-pip'],
 }
