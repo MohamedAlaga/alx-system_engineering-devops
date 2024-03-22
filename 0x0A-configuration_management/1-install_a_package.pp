542cffbadff7229dbb4e129a6054cb1b4ae9880b
MohamedAlaga <malaga.142014@gmail.com>
2024-03-22 10:35:05 +0200

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,8 @@
-#install a flask from pip3
+# Install Flask using pip3
 exec { 'install flask':
     command => 'pip3 install flask==2.1.0',
-    path =>'/usr/bin/'
+    path    => '/usr/bin/',
+    logoutput => true,
+    unless  => 'pip3 show flask | grep -q "Version: 2.1.0"',
+    provider => shell,
 }
