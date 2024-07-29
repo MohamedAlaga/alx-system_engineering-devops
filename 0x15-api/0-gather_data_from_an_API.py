166223863d8976eff2e2b2dc067e47dcfe9892ff
MohamedAlaga <malaga.142014@gmail.com>
2024-07-29 23:23:14 +0300

    API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -14,4 +14,4 @@ if __name__ == "__main__":
     completed = [task.get("title") for task in todos if task.get("completed")]
     print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(task)) for task in completed]
+    [print("\t{}".format(task)) for task in completed]
