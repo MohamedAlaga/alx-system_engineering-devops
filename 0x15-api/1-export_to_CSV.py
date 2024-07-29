533fc4332d3183e0d6fb9900cfdd760faf14697f
MohamedAlaga <malaga.142014@gmail.com>
2024-07-30 00:23:12 +0300

    API

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -10,7 +10,6 @@ if __name__ == "__main__":
     user_id = sys.argv[1]
     user = requests.get("{}/users/{}".format(url, user_id)).json()
     todos = requests.get("{}/todos?userId={}".format(url, user_id)).json()
-    [print("\t {}".format(task)) for task in completed]
     with open("{}.csv".format(user_id), "w") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         [writer.writerow([user_id, user.get("username"),
