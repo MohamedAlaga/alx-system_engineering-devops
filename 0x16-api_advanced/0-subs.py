e92710200d56e986b166babf1251e67ebf17a39a
MohamedAlaga <malaga.142014@gmail.com>
2024-08-16 00:30:09 +0300

    API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,8 +8,7 @@ the number of subscribers for a given subreddit
 
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers for a given subreddit"""
-    x = requests.get("https://www.reddit.com/r/{}/"
-                     + "about.json".format(subreddit),
+    x = requests.get("https://www.reddit.com/r/{}/about.json".format(subreddit),
                      headers={"User-Agent": "Mozilla/5.0"})
     if x.status_code == 200:
         return x.json().get("data").get("subscribers")
